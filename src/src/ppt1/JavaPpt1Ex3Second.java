74731be1e955cf11a629d2018dfca25b24f2311d
parkh <parkh@DESKTOP-CCTGH4F>
2023-09-12 23:11:34 +0900

    homework

--- src/src/ppt1/JavaPpt1Ex3Second.java
+++ src/src/ppt1/JavaPpt1Ex3Second.java
@@ -18,9 +18,9 @@ public class JavaPpt1Ex3Second {
 	}
 	
 	public static boolean isLeapyear(int year) {
-		if((year%4==0 || year%400==0) && (year%100)!=0)
+		if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
 			return true;
-		else 
+		else
 			return false;
 	}
 }
