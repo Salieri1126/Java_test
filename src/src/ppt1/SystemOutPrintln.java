2988d70602a25a603c4ec59b308deb5060275a2a
salieri <pak0302@gmail.com>
2023-09-12 13:58:10 +0900

    second commit

--- src/src/ppt1/SystemOutPrintln.java
+++ src/src/ppt1/SystemOutPrintln.java
@@ -5,6 +5,7 @@ public class SystemOutPrintln {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, 안녕!");
+		System.out.println("Salieri! 안녕!");
 	}
 
 }
