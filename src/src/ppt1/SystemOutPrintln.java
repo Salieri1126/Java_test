01c95556db0c4982f622d6f67c9b542b180061c5
salieri <pak0302@gmail.com>
2023-09-12 13:56:09 +0900

    first commit

---
+++ src/src/ppt1/SystemOutPrintln.java
@@ -0,0 +1,10 @@
+package src.ppt1;
+
+public class SystemOutPrintln {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, 안녕!");
+	}
+
+}
